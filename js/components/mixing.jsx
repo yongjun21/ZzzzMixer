b2f794989397021a2b9806cd687879ed46ced732
tongrhj <tongrhj@gmail.com>
2016-01-13 19:25:34 +0800

    Mixing Bubbles have styles! :rainbow:

--- js/components/mixing.jsx
+++ js/components/mixing.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import s from './mixing.styl'
+import classNames from 'classnames'
+
+import './mixing.styl'
 
 export default class Mixing extends React.Component {
   // static propTypes = {
   //   tracks: React.PropTypes.arrayOf(React.PropTypes.number).isRequired,
   //   samples: React.PropTypes.arrayOf(React.PropTypes.object).isRequired
-  // };
+  // }
 
   constructor (props) {
     super(props)
@@ -24,7 +26,6 @@ export default class Mixing extends React.Component {
       </section>
     )
   }
-
 }
 
 class Bubble extends React.Component {
@@ -32,15 +33,48 @@ class Bubble extends React.Component {
     label: React.PropTypes.string.isRequired
   };
 
+  constructor (props) {
+    super(props)
+    this.state = {
+      selected: false,
+      bubbleSize: 1
+    }
+  }
+
+  handleClick (e) {
+    // const target = React.findDOMNode(this)
+    // target.classList.toggle('Mixing-bubble-animate')
+    if (!this.state.selected) {
+      this.setState({selected: true})
+    } else if (this.state.bubbleSize === 3) {
+      this.setState({
+        selected: false,
+        bubbleSize: 1
+      })
+    }
+    else this.setState({bubbleSize: this.state.bubbleSize + 1})
+    console.log(this.state.selected, this.state.bubbleSize)
+  }
+
   render () {
     const self = this
+
+    let bubbleSize = this.state.bubbleSize
+
+    const bubbleClass = classNames({
+      'Mixing-bubble': true,
+      'Mixing-bubble-selected': this.state.selected,
+      // ['Mixing-bubble-${buttonSize}']: this.state.selected && (this.state.bubbleSize > 1)
+      'Mixing-bubble-2': this.state.selected && (this.state.bubbleSize === 2),
+      'Mixing-bubble-3': this.state.selected && (this.state.bubbleSize === 3)
+    })
 
     const bubbleProps = {
 
     }
 
     return (
-      <div {...bubbleProps} className="Mixing-bubble">
+      <div {...bubbleProps} className={bubbleClass} onClick={this.handleClick.bind(this)}>
         <label>{ self.props.label }</label>
       </div>
     )
