941f53864d1d79fec2ece949d4ed46d9c37688ec
tongrhj <tongrhj@gmail.com>
2016-01-14 02:31:31 +0800

    Added picture icons to mix bubbles

--- js/components/mixing.jsx
+++ js/components/mixing.jsx
@@ -23,7 +23,7 @@ export default class Mixing extends React.Component {
     return (
       <section className='Mixing-section'>
         {this.state.samplenames.map((name, idx) => {
-          return <Bubble label= {name} key= {idx}/>
+          return <Bubble label={name} key={idx}/>
         })
         }
       </section>
@@ -49,6 +49,7 @@ class Bubble extends React.Component {
       this.setState({selected: true})
       this.playSound('open')
     } else if (this.state.bubbleSize === 3) {
+      // Set bubbleSize to 0 then 1 to create a realistic shrinking effect on close
       this.setState({
         selected: false,
         bubbleSize: 0
@@ -87,7 +88,14 @@ class Bubble extends React.Component {
       // ['Mixing-bubble-${buttonSize}']: this.state.selected && (this.state.bubbleSize > 1)
       'Mixing-bubble-0': !this.state.selected && (this.state.bubbleSize === 0),
       'Mixing-bubble-2': this.state.selected && (this.state.bubbleSize === 2),
-      'Mixing-bubble-3': this.state.selected && (this.state.bubbleSize === 3)
+      'Mixing-bubble-3': this.state.selected && (this.state.bubbleSize === 3),
+      'Mixing-bubble-cafe': this.props.label === 'cafe',
+      'Mixing-bubble-fire': this.props.label === 'fire',
+      'Mixing-bubble-jungle': this.props.label === 'jungle',
+      'Mixing-bubble-rain': this.props.label === 'rain',
+      'Mixing-bubble-river': this.props.label === 'river',
+      'Mixing-bubble-seawaves': this.props.label === 'seawaves',
+      'Mixing-bubble-wind': this.props.label === 'wind',
     })
 
     const bubbleProps = {
